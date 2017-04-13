615c957129494dadc0024e13ab2be36df0fe8911
sirougay <sirougay@yahoo.co.jp>
2017-04-13 03:08:11 +0000

    add name to devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,17 @@ class UsersController < ApplicationController
   	@user = current_user
   end
 
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to user_path(@user.id)
+    else
+      render :action => "edit" 
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name)
+    end
 end
